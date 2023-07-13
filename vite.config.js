ae0c8b3ab02393656798010ed68556833c90e95c
Christopher Swinton <chris@theswintons.co.uk>
2023-07-13 17:00:51 +0100

    Revert "npm i vite-plugin-svelte"
    This reverts commit c35f6b56d1c95cf2f57fb4817c44ee138ed1d697.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { svelte } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [
-    svelte({
-      /* plugin options */
-    }),
-  ],
+	plugins: [sveltekit()]
 });
