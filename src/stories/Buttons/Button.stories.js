0a010defe3d755737c6a6b16897f8e27027c8d65
Christopher Swinton <chris@theswintons.co.uk>
2023-07-17 10:52:47 +0100

    Added Colour Palette

--- src/stories/Buttons/Button.stories.js
+++ src/stories/Buttons/Button.stories.js
@@ -1,14 +1,24 @@
-import Button from './Button.svelte';
+import Button from "./Button.svelte";
 
 // More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
 export default {
-  title: 'Components/Button',
+  title: "Components/Button",
   component: Button,
-  tags: ['autodocs'],
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: "Header", value: "#003594" },
+        { name: "Sidebar", value: "#CED4DA" },
+        { name: "Body", value: "#F4F5F6" },
+        { name: "Footer", value: "#FFF" },
+      ],
+    },
+  },
+  tags: ["autodocs"],
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
     size: {
-      control: { type: 'select' },
+      control: { type: "select" },
     },
   },
 };
@@ -17,12 +27,12 @@ export default {
 export const Primary = {
   args: {
     primary: true,
-    label: 'Button',
+    label: "Button",
   },
 };
 
 export const Secondary = {
   args: {
-    label: 'Button',
+    label: "Button",
   },
 };
