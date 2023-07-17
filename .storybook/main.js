be8a44b576727cc8f3cd46f52ea52a5c466d49ca
Christopher Swinton <chris@theswintons.co.uk>
2023-07-17 11:50:32 +0100

    Added less config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,23 @@
 /** @type { import('@storybook/sveltekit').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-docs",
-    '@storybook/addon-a11y',
-    'storybook-addon-themes',
-    '@storybook/preset-scss',
+    "@storybook/addon-a11y",
+    "storybook-addon-themes",
+    "@storybook/preset-scss",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        less: {
+          // Require your Less preprocessor here
+          implementation: require("less"),
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/sveltekit",
